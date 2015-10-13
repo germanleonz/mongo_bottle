394036a75e5b8bbbbd236e6a39ef04972f6289ec
German Leon <gleon@mahisoft.com>
2015-10-13 15:41:34 -0430

    Extended the website with a form, post methods and another view

--- web/app.py
+++ web/app.py
@@ -3,6 +3,8 @@ import pymongo
 
 @bottle.route('/')
 def index():
+    mythings = ['apple', 'orange', 'banana', 'peach']
+
     connection = pymongo.MongoClient('mongo', 27017)
 
     db = connection.test
@@ -11,7 +13,15 @@ def index():
 
     item = name.find_one()
 
-    return bottle.template('index', username = item["first"])
+    return bottle.template('index', {'username' : item["first"], 'things': mythings})
+
+@bottle.post('/favorite_fruit')
+def favorite_fruit():
+    fruit = bottle.request.forms.get("fruit")
+    if (fruit == None or fruit == ""):
+        fruit = "No Fruit Selected"
+
+    return bottle.template('fruit_selection.tpl', {'fruit' : fruit})
 
 bottle.debug(True)
 bottle.run(host='0.0.0.0', port=8080)
--- web/views/index.tpl
+++ web/views/index.tpl
@@ -5,6 +5,16 @@
     <title>Hello World!</title>
 </head>
 <body>
-    Welcome {{username}}
+    <p>Welcome {{username}}</p>
+    <ul>
+        %for thing in things:
+        <li>{{thing}}</li>
+        %end
+    </ul>
+    <form action="/favorite_fruit" method="POST">
+        What is your favorite fruit?
+        <input type="text" name="fruit" size="40" value=""><br>
+        <input type="submit" value="Submit">
+    </form>
 </body>
 </html>
